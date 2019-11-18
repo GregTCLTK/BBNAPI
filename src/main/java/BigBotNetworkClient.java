1073c2b451c4c8e67c0b7e71649993deb6f581e6
GregTCLTK <gregtcltk@gmail.com>
2019-11-18 16:08:26 +0100

    First commit

---
+++ src/main/java/BigBotNetworkClient.java
@@ -0,0 +1,35 @@
+import com.google.gson.JsonObject;
+import okhttp3.*;
+
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * @author GregTCLTK / Skidder
+ */
+
+public class BigBotNetworkClient {
+
+    private final String token;
+
+    public BigBotNetworkClient(String token) {
+        this.token= token;
+    }
+
+    public void sendMessage(String ChannelID, String MessageContent, Color Color) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("content", MessageContent);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), jsonObject.toString());
+
+        Request request = new Request.Builder()
+                .url("https://discordapp.com/api/v6/channels/" + ChannelID + "/messages")
+                .post(body)
+                .addHeader("Authorization", token)
+                .build();
+
+
+        new OkHttpClient().newCall(request).execute().close();
+
+    }
+}
